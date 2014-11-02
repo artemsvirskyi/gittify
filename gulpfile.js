27621c3f87482818f9e4d6703393f6804103eb17
Artem Svirskyi <asv@qapint.com>
2014-11-02 22:01:42 +0200

    add bin to package.json, wrap gulpfile in exports

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
-var gulp = require('gulp'),
-	git = require('gulp-git'),
-	del = require('del'),
-	es = require('event-stream'),
-	params = require('./src/params'),
-	path = require('path');
+// npm install -g git+ssh://git@github.com:SakerONE/gittify.git
 
-gulp.task('clone', function(cb){
-	git.clone(params.remote, {args: params.name, cwd: './gittify'}, cb);
-});
+module.exports = function(gulp){
+	var git = require('gulp-git'),
+		del = require('del'),
+		es = require('event-stream'),
+		params = require('./src/params'),
+		path = require('path');
 
-gulp.task('clean', ['move'], function(cb){
-	del(['gittify/**', '!gittify'], {force: true}, cb);
-});
+	gulp.task('clone', function(cb){
+		git.clone(params.remote, {args: params.name, cwd: './gittify'}, cb);
+	});
 
-gulp.task('move', ['clone'], function(){
-	return gulp.src('.git/**', {cwd: path.join('gittify', params.name)})
-			   .pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name, '.git')));
-});
+	gulp.task('clean', ['move'], function(cb){
+		del(['gittify/**', '!gittify'], {force: true}, cb);
+	});
 
-gulp.task('default', ['clean'], function(cb){
-	console.log('done');
-});
+	gulp.task('move', ['clone'], function(){
+		return gulp.src('.git/**', {cwd: path.join('gittify', params.name)})
+				   .pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name, '.git')));
+	});
+
+	gulp.task('default', ['clean'], function(cb){
+		console.log('done');
+	});
+};
