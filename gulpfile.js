6b73a003751d00a4eba7b8fbed9a95d72f714734
Artem Svirskyi <asv@qapint.com>
2014-11-09 00:07:40 +0200

    update(gulpfile.js): path fixes

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,16 @@ module.exports = function(gulp){
 		path = require('path');
 
 	gulp.task('clone', function(cb){
-		git.clone(params.remote, {args: params.name, cwd: './gittify'}, cb);
-	});
-
-	gulp.task('clean', ['move'], function(cb){
-		del(['gittify/**', '!gittify'], {force: true}, cb);
+		git.clone(params.remote, {args: params.name, cwd: path.join(__dirname, 'gittify')}, cb);
 	});
 
 	gulp.task('move', ['clone'], function(){
-		return gulp.src('.git/**', {cwd: path.join('gittify', params.name)})
-				   .pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name, '.git')));
+		return gulp.src('.git/**', {cwd: path.join(__dirname, 'gittify', params.name)})
+			.pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name, '.git')));
+	});
+
+	gulp.task('clean', ['move'], function(cb){
+		del(['gittify/**', '!gittify'], {force: true, cwd: __dirname}, cb);
 	});
 
 	gulp.task('default', ['clean'], function(cb){
