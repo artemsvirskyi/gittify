a5fa629dbb5b559b47c34c1afeff31170abd77ba
Artem Svirskyi <asv@qapint.com>
2014-11-09 21:55:03 +0200

    add(gitignore): adds .bower.json in gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ module.exports = function(gulp){
 		del = require('del'),
 		es = require('event-stream'),
 		params = require('./src/params'),
-		path = require('path');
+		path = require('path'),
+		concat = require('gulp-concat');
 
 	gulp.task('clone', function(cb){
 		git.clone(params.remote, {args: params.name, cwd: path.join(__dirname, 'gittify')}, cb);
@@ -16,7 +17,13 @@ module.exports = function(gulp){
 			.pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name, '.git')));
 	});
 
-	gulp.task('clean', ['move'], function(cb){
+	gulp.task('gitignore', ['clone'], function(){
+		return gulp.src(['./src/.default-gitignore', path.join('./gittify', params.name, '.gitignore')], {cwd: __dirname})
+			.pipe(concat('.gitignore'))
+			.pipe(gulp.dest(path.join(process.cwd(), params.dir, params.name)));
+	});
+
+	gulp.task('clean', ['move', 'gitignore'], function(cb){
 		del(['gittify/**', '!gittify'], {force: true, cwd: __dirname}, cb);
 	});
 
