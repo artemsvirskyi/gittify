b94c05f380cb9b85e686ae29c2322b776f5c54ea
Artem Svirsky <asv@qapint.com>
2014-11-11 09:46:25 +0200

    update(merge): add trim

--- src/merge.js
+++ src/merge.js
@@ -3,7 +3,7 @@ var es = require('event-stream');
 module.exports = function(){
 	return es.map(function(file, cb){
 		var str = file.contents.toString(),
-			lines = str.split('\n').filter(isUnique);
+			lines = str.split('\n').map(trim).filter(isUnique);
 
 		file.contents = new Buffer(lines.join('\n'));
 
@@ -13,4 +13,8 @@ module.exports = function(){
 
 function isUnique(item, index, array){
 	return array.indexOf(item) === index;
+}
+
+function trim(str){
+	return str.trim();
 }
